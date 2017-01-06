b3c84388325dc2511e45020e7666c8852d684d98
Sonia Martinez <soniamartinezctr@gmail.com>
2017-01-06 14:23:20 -0600

    Integrar título en headers y favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html>
     <head>
+          <title>@yield('titleMexicoX','MéxicoX')</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
         <script src="{{ asset('js/efectos.js') }}"></script>
         <!--<link rel="stylesheet" href="css/bootstrap.min.css">-->
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/principal.css') }}">
-        <link href='https://fonts.googleapis.com/css?family=Play' rel='stylesheet' type='text/css'>
-        <meta charset= "utf-8">
+        <link href='https://fonts.googleapis.com/css?family=Play' rel='stylesheet' type='text/css'>        
+        <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" >
         <style>
             .fit {
                 max-width: 100%;
